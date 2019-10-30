c9311db4012bbcf83803204f518dff5d02f9f632
Kashif Ghaffar Asim <kashif.ghaffar100@gmail.com>
2019-10-30 16:31:36 +0500

    Exist

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,20 @@
 class GamesController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
+  # All Soccer 
 	def index 
 		@soccers = Soccer.all
 	end 
 
 
+  # Soccer  Update 
 	def update 
 		@soccer = Soccer.find_by_id(params[:id])
 
 		metod = params[:metod] == "increase" ? @soccer.minutes_booked.to_i + 1 : @soccer.minutes_booked.to_i - 1
 		if @soccer.update(minutes_booked: metod) 
-		else
 
 		end
-
 	end 
+  
 end
